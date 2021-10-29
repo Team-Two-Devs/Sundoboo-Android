dabc5f31dd0e39e677f76f64728d67d166e113a8
HyeonSeokGo <gohyeonseok02@gmail.com>
2021-10-29 19:59:23 +0900

    Dependiencies.kt, Versions.kt 파일 추가.

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+object Dependencies {
+    const val CORE_KTX = "androidx.core:core-ktx:1.7.0"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.3.1"
+
+    //layout
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.1"
+
+    //material
+    const val MATERIAL = "com.google.android.material:material:1.4.0"
+
+    //junit
+    const val JUNIT = "junit:junit:4.+"
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
+
+    //espresso
+    const val ESPRESSO = "androidx.test.espresso:espresso-core:3.4.0"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val GRADLE = "7.0.3"
+
+    const val KOTLIN = "1.5.31"
+
+    const val MIN_SDK = 24
+    const val COMPILE_SDK = 31
+    const val TARGET_SDK = 31
+}
