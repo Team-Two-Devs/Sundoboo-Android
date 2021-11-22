892f2cc07c03944e0d088ecdef41188c3f391e72
HyeonSeokGo <gohyeonseok02@gmail.com>
2021-11-22 22:15:52 +0900

    Feature : FeedDetailFragment 생성 및 DataBinding 설정.
    FeedDetailFragment에 사용되는 menu_feed_detail 생성.

---
+++ app/src/main/java/com/example/sundoboo/feed/model/Feed.kt
@@ -0,0 +1,11 @@
+package com.example.sundoboo.feed.model
+
+import com.example.sundoboo.vote.Vote
+
+data class Feed(
+    val id: String,
+    val title: String,
+    val description: String,
+    val votes: List<Vote> = listOf(),
+    //TODO 2021.11.12 작성자를 표시하는 writer: Writer 클래스 추가하기
+)
