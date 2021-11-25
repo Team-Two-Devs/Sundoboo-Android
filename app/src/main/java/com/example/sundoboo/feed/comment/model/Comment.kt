4477daeed35c86c5a5a5d1f662c4c1d0b6d4b651
HyeonSeokGo <gohyeonseok02@gmail.com>
2021-11-25 23:38:03 +0900

    Todo 추가.

--- app/src/main/java/com/example/sundoboo/feed/comment/model/Comment.kt
+++ app/src/main/java/com/example/sundoboo/feed/comment/model/Comment.kt
@@ -8,5 +8,6 @@ data class Comment(
     val date: String,
     val writer: Writer,
 ) {
+    //Todo (2021.11.25) 프로퍼티로 변경할까 고민 중...
     fun getWriterName() = writer.getName()
 }
--- app/src/main/java/com/example/sundoboo/user/model/Writer.kt
+++ app/src/main/java/com/example/sundoboo/user/model/Writer.kt
@@ -3,6 +3,7 @@ package com.example.sundoboo.user.model
 data class Writer(
     val user: User
 ) {
+    //Todo (2021.11.25) 프로퍼티로 변경할까 고민 중...
     fun getId() = user.id
     fun getName() = user.name
 }
