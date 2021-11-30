5051cdf59b28f483f1634874e8b7eadbdfaa1bc8
HyeonSeokGo <gohyeonseok02@gmail.com>
2021-11-30 20:30:24 +0900

    Todo : (2021.11.25) 프로퍼티로 변경할까 고민 중... 완료

--- app/src/main/java/com/example/sundoboo/user/model/Writer.kt
+++ app/src/main/java/com/example/sundoboo/user/model/Writer.kt
@@ -3,7 +3,6 @@ package com.example.sundoboo.user.model
 data class Writer(
     val user: User
 ) {
-    //Todo (2021.11.25) 프로퍼티로 변경할까 고민 중...
-    fun getId() = user.id
-    fun getName() = user.name
+    val id : String get() = user.id
+    val name: String get() = user.name
 }
